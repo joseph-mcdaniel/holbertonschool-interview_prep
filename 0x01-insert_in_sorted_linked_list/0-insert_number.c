e34aff918b23b6fddbabb1eb749a7fb2328404d2
joseph-mcdaniel <joseph.mcdaniel@holbertonschool.com>
2018-05-27 20:31:29 +0000

    init commit

---
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,54 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * insert_node - inserts a number into a sorted
+ * singly linked list
+ * @head: head node
+ * @number: value to be inserted
+ *
+ * Return: address of new node
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *node, *temp;
+
+	node = malloc(sizeof(listint_t));
+
+	if (node == NULL)
+		return (NULL);
+
+	node->n = number;
+	node->next = NULL;
+	/* add node at beginning of list */
+	if (node->n < (*head)->n)
+	{
+		node->next = *head;
+		*head = node;
+		return (node);
+	}
+	/* add node to empty list */
+	if (*head == NULL)
+	{
+		*head = node;
+		return (node);
+	}
+
+	/*traverse and add node in sorted order*/
+	temp = *head;
+	while (temp->next != NULL)
+	{
+		if (number > temp->next->n)
+		{
+			temp = temp->next;
+		}
+		else
+		{
+			node->next = temp->next;
+			temp->next = node;
+			return (node);
+		}
+	}
+	temp->next = node;
+	return (node);
+}
---
+++ 0x01-insert_in_sorted_linked_list/README.md
@@ -0,0 +1,3 @@
+# Insert in sorted linked list
+
+## Write a function in C that inserts a number into a sorted singly linked list.
